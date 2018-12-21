155140935589d06b0ff50f15db9ed4a7044c8a66
Algy <algy@ubuntu.ubuntu-domain>
2018-12-22 04:50:27 +0900

    Add setup.py and LICENSE

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup, Command
+
+required = [
+    'pillow',
+    'numpy',
+    'python-dateutil',
+]
+
+setup(
+    name="pipex",
+    version="0.0.1",
+    description="Data Processing With Pipe Syntax",
+    author="Alchan Algy Kim",
+    author_email="a9413miky@gmail.com",
+    url="https://github.com/Algy/pipex",
+    packages=find_packages(exclude=["test", "tess.*"]),
+    entry_points={},
+    package_data={
+        "": ["LICENSE"],
+    },
+    python_requires=">=3.5",
+    install_requires=required,
+    extras_require={},
+    include_package_data=True,
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+)
